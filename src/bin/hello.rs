30ef6474ce651629e1adaded5ea99d54ae47e8f4
Jonah Dahlquist <hi@jonah.name>
2021-04-20 09:04:53 -0700

    Generated from Knurling app template, configured for target, added dependencies, and memory.x

---
+++ src/bin/hello.rs
@@ -0,0 +1,11 @@
+#![no_main]
+#![no_std]
+
+use rollova as _; // global logger + panicking-behavior + memory layout
+
+#[cortex_m_rt::entry]
+fn main() -> ! {
+    defmt::info!("Hello, world!");
+
+    rollova::exit()
+}
